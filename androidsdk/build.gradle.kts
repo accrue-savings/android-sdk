37160ece0eaea0adc86dd35469ba088870fd47bf
othecos <othecos@gmail.com>
2025-01-28 14:13:35 -0300

    chore: bump version

--- androidsdk/build.gradle.kts
+++ androidsdk/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     group = "com.accruesavings.androidsdk"
-    version = "v1.0.10"
+    version = "v1.0.11"
     namespace = "com.accruesavings.androidsdk"
     compileSdk = 34
 
@@ -73,7 +73,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.accruesavings"
             artifactId = "androidsdk"
-            version = "v1.0.10"
+            version = "v1.0.11"
 
             afterEvaluate {
                 from(components["release"])
