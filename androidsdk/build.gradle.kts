87eb56a12726e978b5a210a3ae35f74723d98831
othecos <othecos@gmail.com>
2025-07-14 12:12:16 -0300

    Revert "build: updating package version to v1.0.2 [skip ci]"
    This reverts commit 9a5c77c5931724f6700b1af5485e198822641a1a.

--- androidsdk/build.gradle.kts
+++ androidsdk/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     group = "com.accruesavings.androidsdk"
-    version = "v1.0.2"
+    version = "v1.2.0"
     namespace = "com.accruesavings.androidsdk"
     compileSdk = 34
 
@@ -88,7 +88,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.accruesavings"
             artifactId = "androidsdk"
-            version = "v1.0.2"
+            version = "v1.2.0"
 
             afterEvaluate {
                 from(components["release"])
@@ -116,7 +116,7 @@ publishing {
 //
 //                groupId = "com.github.accrue-savings"
 //                artifactId = "android-sdk"
-//                version = "v1.0.2"
+//                version = "v1.0.15"
 //
 //                afterEvaluate {
 //                    from(components["release"])
