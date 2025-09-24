455070e6d1efb9913533a8f1fd8565b2ca8235a2
Github Workflow <githubworkflow@accruemoney.com>
2025-09-24 19:22:35 +0000

    build: updating package version to v1.2.5 [skip ci]

--- androidsdk/build.gradle.kts
+++ androidsdk/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     group = "com.accruesavings.androidsdk"
-    version = "v1.3.0"
+    version = "v1.2.5"
     namespace = "com.accruesavings.androidsdk"
     compileSdk = 34
 
@@ -89,7 +89,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.accruesavings"
             artifactId = "androidsdk"
-            version = "v1.3.0"
+            version = "v1.2.5"
 
             afterEvaluate {
                 from(components["release"])
@@ -116,7 +116,7 @@ publishing {
 //
 //                groupId = "com.github.accrue-savings"
 //                artifactId = "android-sdk"
-//                version = "v1.2.4"
+//                version = "v1.2.5"
 //
 //                afterEvaluate {
 //                    from(components["release"])
